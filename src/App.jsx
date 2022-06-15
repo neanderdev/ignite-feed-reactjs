b33dd438033c4d770353433aa33d7e4adc96545f
neanderdev <neander.faria@hotmail.com>
2022-06-15 10:38:17 -0300

    Creating the Application Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css';
+import './global.css';
 
 export default function App() {
   return (
     <div>
-      <Header />
+      <Header />   
 
-      <Post 
-        author="Neander de Souza" 
-        content="Conteúdo do dia 15/06/2022, venha conhecer as novidade" 
-      />
-      <Post 
-        author="Neander de Souza" 
-        content="Conteúdo do dia 14/06/2022, venha conhecer as novidade" 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post 
+            author="Neander de Souza" 
+            content="Conteúdo do dia 15/06/2022, venha conhecer as novidade" 
+          />
+
+          <Post 
+            author="Neander de Souza" 
+            content="Conteúdo do dia 14/06/2022, venha conhecer as novidade" 
+          />
+        </main> 
+      </div> 
     </div>
   )
 }
+
