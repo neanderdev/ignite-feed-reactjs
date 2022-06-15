045de16353ea43b3b78adff66416195e7c108e8e
neanderdev <neander.faria@hotmail.com>
2022-06-15 09:54:12 -0300

    Seeing the Property Concept in Component in ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Post } from "./Post";
 export default function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Neander de Souza" 
+        content="Conteúdo do dia 15/06/2022, venha conhecer as novidade" 
+      />
+      <Post 
+        author="Neander de Souza" 
+        content="Conteúdo do dia 14/06/2022, venha conhecer as novidade" 
+      />
     </div>
   )
 }
