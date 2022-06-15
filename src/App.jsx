b120a711e53eb7c0aae7692e7d36f1bfb85bb904
neanderdev <neander.faria@hotmail.com>
2022-06-15 16:04:40 -0300

    Iterating in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Post } from "./components/Post";
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+      id: 1,
+      author: {
+          avatarUrl: 'https://www.github.com/neanderdev.png',
+          name: 'Neander de Souza',
+          role: 'Web Developer',
+      },
+      content: [
+          { type: 'paragraph', content: 'Fala galeraa 👋' },
+          { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+          { type: 'link', content: 'jane.design/doctorcare'},
+      ],
+      publishedAt: new Date('2022-06-15 08:30:15'),
+  },
+  {
+      id: 2,
+      author: {
+          avatarUrl: 'https://www.github.com/neanderdev.png',
+          name: 'Neander de Souza',
+          role: 'Web Developer',
+      },
+      content: [
+          { type: 'paragraph', content: 'Fala galeraa 👋' },
+          { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+          { type: 'link', content: 'jane.design/doctorcare'},
+      ],
+      publishedAt: new Date('2022-06-16 08:30:15'),
+  }
+];
+
 export default function App() {
   return (
     <div>
@@ -14,9 +45,14 @@ export default function App() {
         <Sidebar />
 
         <main>
-          <Post />
-
-          <Post />
+          {posts.map((post) => (
+            <Post 
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main> 
       </div> 
     </div>
