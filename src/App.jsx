7a951802abcb77a41ea89d118c54e38aab5dcefc
neanderdev <neander.faria@hotmail.com>
2022-06-15 10:02:53 -0300

    Selling CSS Modules in ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { Post } from "./Post";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
 
 export default function App() {
   return (
     <div>
+      <Header />
+
       <Post 
         author="Neander de Souza" 
         content="Conteúdo do dia 15/06/2022, venha conhecer as novidade" 
